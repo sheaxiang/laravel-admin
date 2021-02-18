9b9c4f72a2be6b6ab395bd030e990d07a7488f2d
sheaxiang <642290531@qq.com>
2021-02-18 11:04:04 +0800

    更改目录名backend=>admin

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -87,21 +87,21 @@ class AdminServiceProvider extends ServiceProvider
 
         //发布前端代码源码文件
         $this->publishes([
-            __DIR__.'/../resources/admin' => base_path('backend'),
+            __DIR__.'/../resources/admin' => base_path('admin'),
         ], 'laravel-admin-resources');
 
         // 更新扩展包时需要更新的
         $this->addPublishGroup('laravel-admin-resources-force', [
-            __DIR__.'/../resources/admin/config/routes.ts' => base_path('backend/config/routes.ts'),
-            __DIR__.'/../resources/admin/dist' => base_path('backend/dist'),
-            __DIR__.'/../resources/admin/public' => base_path('backend/public'),
-            __DIR__.'/../resources/admin/src' => base_path('backend/src'),
-            __DIR__.'/../resources/admin/dist' => public_path('backend'),
+            __DIR__.'/../resources/admin/config/routes.ts' => base_path('admin/config/routes.ts'),
+            __DIR__.'/../resources/admin/dist' => base_path('admin/dist'),
+            __DIR__.'/../resources/admin/public' => base_path('admin/public'),
+            __DIR__.'/../resources/admin/src' => base_path('admin/src'),
+            __DIR__.'/../resources/admin/dist' => public_path('admin'),
         ]);
 
-        //发布前端打包好的代码到backend目录
+        //发布前端打包好的代码到admin目录
         $this->publishes([
-            __DIR__.'/../resources/admin/dist' => public_path('backend'),
+            __DIR__.'/../resources/admin/dist' => public_path('admin'),
         ], 'laravel-admin-resources-dist');
 
     }
